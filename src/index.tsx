27ba476c43dffc101240012c2d9e13f21f539982
pedroaal <pedroaal04@gmail.com>
2024-07-17 10:22:31 -0500

    fix base code

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web"
 import App from "./App"
+import "./style/index.scss"
 
 render(() => <App />, document.getElementById("root") as HTMLElement)
