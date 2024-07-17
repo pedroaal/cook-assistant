2f2d8c99d36023c98fe9c4a72d224ac9b752bba6
pedroaal <pedroaal04@gmail.com>
2024-07-17 17:38:16 -0500

    added main screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Router } from "@solidjs/router"
 
 import routes from "~/config/routes"
 import { CoreProvider } from "~/context/core.context"
+
 import Alerts from "./components/core/Alerts"
 import Loader from "./components/core/Loader"
 
@@ -10,8 +11,7 @@ function App() {
     <CoreProvider>
       <Alerts />
       <Loader />
-      <div class="container mx-auto p-5">
-        <header class="text-center">Tauri - Solid</header>
+      <div class="container mx-auto p-4 bg-base-100 text-base-content h-dvh w-dvw">
         <Router>{routes}</Router>
       </div>
     </CoreProvider>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,45 @@
 import type { Component } from "solid-js"
+import {
+  HiSolidEye,
+  HiSolidMicrophone,
+  HiSolidPaperAirplane,
+} from "solid-icons/hi"
+
+import Button from "~/components/shared/Button"
 
 const Home: Component = () => {
-  return <div>Home</div>
+  const voice = () => {}
+  const vision = () => {}
+
+  return (
+    <div class="flex flex-col h-full gap-4">
+      <div class="flex-1 flex flex-col gap-2 justify-center items-center">
+        <Button
+          title="Voice"
+          onClick={voice}
+          icon={<HiSolidMicrophone class="size-6 text-primary" />}
+          class="btn-outline btn-lg btn-wide"
+        />
+        <Button
+          title="Vision"
+          onClick={vision}
+          icon={<HiSolidEye class="size-6 text-primary" />}
+          class="btn-outline btn-lg btn-wide"
+        />
+      </div>
+      <div class="flex gap-2">
+        <input
+          type="text"
+          name="search"
+          placeholder="Tell me anything..."
+          class="input input-bordered input-primary w-full"
+        />
+        <button class="btn btn-primary" type="button">
+          <HiSolidPaperAirplane class="size-6 text-primary-content" />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
